1f5e7d8b65e1e7978bb4fc49df2e1e4e03e64b18
vinukumar-vs <vinukumar_vs@yahoo.com>
2025-06-18 00:56:43 +0530

    Jmeter report for Get API call

--- src/api/sequelizeRoutes.js
+++ src/api/sequelizeRoutes.js
@@ -5,6 +5,9 @@ const { Trip, Customer, Driver, Vehicle } = require('../models');
 router.post('/trip', async (req, res) => {
   try {
     const { customer_id, driver_id, vehicle_id, start_time, end_time } = req.body;
+    // console.log('Received trip data:', req.body);
+    // Validate required fields
+    // If any of these fields are undefined or empty, return a 400 error
     if (
       customer_id === undefined ||
       driver_id === undefined ||
@@ -12,16 +15,18 @@ router.post('/trip', async (req, res) => {
       !start_time ||
       !end_time
     ) {
-      return res.status(400).json({ error: 'Missing required fields' });
+      return res.status(400).json({ error: 
+        `Missing required fields: ${customer_id}, ${driver_id}, ${vehicle_id}, ${start_time}, ${end_time}`
+       });
     }
     const [customer, driver, vehicle] = await Promise.all([
       Customer.findByPk(customer_id),
       Driver.findByPk(driver_id),
       Vehicle.findByPk(vehicle_id),
     ]);
-    if (!customer) return res.status(400).json({ error: 'Invalid customer_id' });
-    if (!driver) return res.status(400).json({ error: 'Invalid driver_id' });
-    if (!vehicle) return res.status(400).json({ error: 'Invalid vehicle_id' });
+    if (!customer) return res.status(400).json({ error: `Invalid customer_id customer - ${customer}` });
+    if (!driver) return res.status(400).json({ error: `Invalid driver_id driver - ${driver}` });
+    if (!vehicle) return res.status(400).json({ error: `Invalid driver_id vehicle - ${vehicle}` });
 
     const trip = await Trip.create({ customer_id, driver_id, vehicle_id, start_time, end_time });
     res.status(201).json(trip);
